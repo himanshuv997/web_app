b9f5df19ea3f63c88fdedc1d0eb4fb85e9165f15
Himanshu Verma <55078501+himanshuv997@users.noreply.github.com>
2022-05-19 21:40:42 +0530

    Add files via upload

---
+++ web_app/app.py
@@ -0,0 +1,337 @@
+import os
+#--------------------------------------------------------------------------------------------------------
+from flask import Flask, flash, redirect,render_template,request, url_for
+from werkzeug.utils import secure_filename
+#--------------------------------------------------------------------------------------------------------
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+from keras.models import Model
+from keras.layers.merge import Add
+from keras.utils import conv_utils
+from keras.layers.core import Lambda
+from keras.layers.core import Dropout
+from tensorflow.keras.layers import Layer, InputSpec
+from tensorflow.keras.layers import Input, UpSampling2D
+from tensorflow.keras.layers import Conv2D, Activation, BatchNormalization
+#--------------------------------------------------------------------------------------------------------
+
+
+# the paper defined hyper-parameter:chr
+channel_rate = 64
+# Note the image_shape must be multiple of patch_shape
+image_shape = (256, 256, 3)
+patch_shape = (channel_rate, channel_rate, 3)
+
+ngf = 64
+ndf = 64
+input_nc = 3
+output_nc = 3
+input_shape_generator = (256, 256, input_nc)
+input_shape_discriminator = (256, 256, output_nc)
+n_blocks_gen = 9
+RESHAPE = (256,256)
+
+
+
+
+
+def res_block(input, filters, kernel_size=(3, 3), strides=(1, 1), use_dropout=False):
+    """
+    Instanciate a Keras Resnet Block using sequential API.
+
+    :param input: Input tensor
+    :param filters: Number of filters to use
+    :param kernel_size: Shape of the kernel for the convolution
+    :param strides: Shape of the strides for the convolution
+    :param use_dropout: Boolean value to determine the use of dropout
+    :return: Keras Model
+    """
+    x = ReflectionPadding2D((1, 1))(input)
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=strides,)(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    if use_dropout:
+        x = Dropout(0.5)(x)
+
+    x = ReflectionPadding2D((1, 1))(x)
+    x = Conv2D(filters=filters,
+               kernel_size=kernel_size,
+               strides=strides,)(x)
+    x = BatchNormalization()(x)
+
+    merged = Add()([input, x])
+    return merged
+
+
+def spatial_reflection_2d_padding(x, padding=((1, 1), (1, 1)), data_format=None):
+    """
+    Pad the 2nd and 3rd dimensions of a 4D tensor.
+
+    :param x: Input tensor
+    :param padding: Shape of padding to use
+    :param data_format: Tensorflow vs Theano convention ('channels_last', 'channels_first')
+    :return: Tensorflow tensor
+    """
+    assert len(padding) == 2
+    assert len(padding[0]) == 2
+    assert len(padding[1]) == 2
+    if data_format is None:
+        data_format = image_data_format()
+    if data_format not in {'channels_first', 'channels_last'}:
+        raise ValueError('Unknown data_format ' + str(data_format))
+
+    if data_format == 'channels_first':
+        pattern = [[0, 0],
+                   [0, 0],
+                   list(padding[0]),
+                   list(padding[1])]
+    else:
+        pattern = [[0, 0],
+                   list(padding[0]), list(padding[1]),
+                   [0, 0]]
+    return tf.pad(x, pattern, "REFLECT")
+
+
+# TODO: Credits
+class ReflectionPadding2D(Layer):
+    """Reflection-padding layer for 2D input (e.g. picture).
+    This layer can add rows and columns or zeros
+    at the top, bottom, left and right side of an image tensor.
+    # Arguments
+        padding: int, or tuple of 2 ints, or tuple of 2 tuples of 2 ints.
+            - If int: the same symmetric padding
+                is applied to width and height.
+            - If tuple of 2 ints:
+                interpreted as two different
+                symmetric padding values for height and width:
+                `(symmetric_height_pad, symmetric_width_pad)`.
+            - If tuple of 2 tuples of 2 ints:
+                interpreted as
+                `((top_pad, bottom_pad), (left_pad, right_pad))`
+        data_format: A string,
+            one of `channels_last` (default) or `channels_first`.
+            The ordering of the dimensions in the inputs.
+            `channels_last` corresponds to inputs with shape
+            `(batch, height, width, channels)` while `channels_first`
+            corresponds to inputs with shape
+            `(batch, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+    # Input shape
+        4D tensor with shape:
+        - If `data_format` is `"channels_last"`:
+            `(batch, rows, cols, channels)`
+        - If `data_format` is `"channels_first"`:
+            `(batch, channels, rows, cols)`
+    # Output shape
+        4D tensor with shape:
+        - If `data_format` is `"channels_last"`:
+            `(batch, padded_rows, padded_cols, channels)`
+        - If `data_format` is `"channels_first"`:
+            `(batch, channels, padded_rows, padded_cols)`
+    """
+
+    def __init__(self,
+                 padding=(1, 1),
+                 data_format=None,
+                 **kwargs):
+        super(ReflectionPadding2D, self).__init__(**kwargs)
+        self.data_format = conv_utils.normalize_data_format(data_format)
+        if isinstance(padding, int):
+            self.padding = ((padding, padding), (padding, padding))
+        elif hasattr(padding, '__len__'):
+            if len(padding) != 2:
+                raise ValueError('`padding` should have two elements. '
+                                 'Found: ' + str(padding))
+            height_padding = conv_utils.normalize_tuple(padding[0], 2,
+                                                        '1st entry of padding')
+            width_padding = conv_utils.normalize_tuple(padding[1], 2,
+                                                       '2nd entry of padding')
+            self.padding = (height_padding, width_padding)
+        else:
+            raise ValueError('`padding` should be either an int, '
+                             'a tuple of 2 ints '
+                             '(symmetric_height_pad, symmetric_width_pad), '
+                             'or a tuple of 2 tuples of 2 ints '
+                             '((top_pad, bottom_pad), (left_pad, right_pad)). '
+                             'Found: ' + str(padding))
+        self.input_spec = InputSpec(ndim=4)
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_first':
+            if input_shape[2] is not None:
+                rows = input_shape[2] + self.padding[0][0] + self.padding[0][1]
+            else:
+                rows = None
+            if input_shape[3] is not None:
+                cols = input_shape[3] + self.padding[1][0] + self.padding[1][1]
+            else:
+                cols = None
+            return (input_shape[0],
+                    input_shape[1],
+                    rows,
+                    cols)
+        elif self.data_format == 'channels_last':
+            if input_shape[1] is not None:
+                rows = input_shape[1] + self.padding[0][0] + self.padding[0][1]
+            else:
+                rows = None
+            if input_shape[2] is not None:
+                cols = input_shape[2] + self.padding[1][0] + self.padding[1][1]
+            else:
+                cols = None
+            return (input_shape[0],
+                    rows,
+                    cols,
+                    input_shape[3])
+
+    def call(self, inputs):
+        return spatial_reflection_2d_padding(inputs,
+                                             padding=self.padding,
+                                             data_format=self.data_format)
+
+    def get_config(self):
+        config = {'padding': self.padding,
+                  'data_format': self.data_format}
+        base_config = super(ReflectionPadding2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+def generator_model():
+    """Build generator architecture."""
+    # Current version : ResNet block
+    inputs = Input(shape=image_shape)
+
+    x = ReflectionPadding2D((3, 3))(inputs)
+    x = Conv2D(filters=ngf, kernel_size=(7, 7), padding='valid')(x)
+    x = BatchNormalization()(x)
+    x = Activation('relu')(x)
+
+    n_downsampling = 2
+    for i in range(n_downsampling):
+        mult = 2**i
+        x = Conv2D(filters=ngf*mult*2, kernel_size=(3, 3), strides=2, padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+    mult = 2**n_downsampling
+    for i in range(n_blocks_gen):
+        x = res_block(x, ngf*mult, use_dropout=True)
+
+    for i in range(n_downsampling):
+        mult = 2**(n_downsampling - i)
+        # x = Conv2DTranspose(filters=int(ngf * mult / 2), kernel_size=(3, 3), strides=2, padding='same')(x)
+        x = UpSampling2D()(x)
+        x = Conv2D(filters=int(ngf * mult / 2), kernel_size=(3, 3), padding='same')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+
+    x = ReflectionPadding2D((3, 3))(x)
+    x = Conv2D(filters=output_nc, kernel_size=(7, 7), padding='valid')(x)
+    x = Activation('tanh')(x)
+
+    outputs = Add()([x, inputs])
+    outputs = Lambda(lambda z: z/2)(outputs)
+
+    model = Model(inputs=inputs, outputs=outputs, name='Generator')
+    return model
+
+def load_image(path):
+    img = Image.open(path)
+    return img
+
+def preprocess_image(cv_img):
+    cv_img = cv_img.resize(RESHAPE)
+    img = np.array(cv_img)
+    img = (img - 127.5) / 127.5
+    return img
+
+#numpy                     1.19.5           py39h1f3b974_2    conda-forge
+def deprocess_image(img):
+    img = img * 127.5 + 127.5
+    return img.astype('uint8')
+
+
+#--------------------------------------------------------------------------------------------------------
+
+upload_location = "/Users/himanshuverma/Desktop/SEM6/MinorProject/web_app/static/uploads"
+app = Flask(__name__)
+app.secret_key = 'super secret'
+app.config['UPLOAD_FOLDER'] = upload_location
+app.config['DEBUG'] = True
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+def allowed_file(filename):
+   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/home")
+def home():
+    return render_template("upload_image.html")
+
+
+
+@app.route("/uploader", methods = ['GET', 'POST'])
+def uploader():
+
+    weight_path = '/Users/himanshuverma/Desktop/SEM6/MinorProject/deblur_gan/generator.h5'
+    input_dir='/Users/himanshuverma/Desktop/SEM6/MinorProject/web_app/static/uploads' 
+    output_dir='/Users/himanshuverma/Desktop/SEM6/MinorProject/web_app/static/result'
+
+    for f in os.listdir(input_dir):
+        os.remove(os.path.join(input_dir, f))
+
+
+    if request.method == 'POST' :
+        if 'file1' not in request.files:
+           flash('No file attached in request','danger')
+           return redirect(url_for("home"))
+
+        
+        image_file = request.files['file1']
+        
+        if image_file.filename=="":
+            flash("Image must have a filename",'danger')
+            return redirect(url_for("home"))
+        
+        if not allowed_file(image_file.filename):
+            flash("Image extension not supported",'danger')
+            return redirect(url_for("home"))
+        else:
+            image_file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_filename(image_file.filename)))
+        flash(''' 
+                    image uploaded
+                  Result shown below 
+        ''',"success")
+
+        
+    
+    g = generator_model()
+    g.load_weights(weight_path)
+    for image_name in os.listdir(input_dir):
+        image = np.array([preprocess_image(load_image(os.path.join(input_dir, image_name)))])
+        x_test = image
+        generated_images = g.predict(x=x_test)
+        generated = np.array([deprocess_image(img) for img in generated_images])
+        x_test = deprocess_image(x_test)
+        for i in range(generated_images.shape[0]):
+            x = x_test[i, :, :, :]
+            img = generated[i, :, :, :]
+            output = np.concatenate((x, img), axis=1)
+            im = Image.fromarray(output.astype(np.uint8))
+            im.save(os.path.join(output_dir, image_name))
+
+    return render_template("output_image.html",output=image_file.filename)
+
+
+if __name__ == "__main__":
+    app.run()
+
